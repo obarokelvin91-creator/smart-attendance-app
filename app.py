e48f3d34c5058aff3eaab1692627db04148e143f
Obaro Kelvin <obarokelvin91@gmail.com>
2025-12-24 02:28:35 +0100

    Render deployment ready

--- app.py
+++ app.py
@@ -340,5 +340,8 @@ def report_csv():
     )
 
 # ---------------- RUN ----------------
-if __name__=="__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
